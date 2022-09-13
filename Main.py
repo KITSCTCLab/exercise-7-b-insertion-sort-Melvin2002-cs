4aefaef08de8c4300b88fd3289f2753e20e439fb
Melvin2002-cs <110010657+Melvin2002-cs@users.noreply.github.com>
2022-09-13 12:00:56 +0530

    3rd try

--- Main.py
+++ Main.py
@@ -18,7 +18,7 @@ def insertionSort(array) -> List[int]:
         if array[i]<array[j]:
           swap(array,i,j)
           execution=1
-       else:
+        else:
           array[j+1]=array[j]
   return array
       
