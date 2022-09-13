b2c9f03b054b92c93b76ebd6f7f36fce0e294d25
Melvin2002-cs <110010657+Melvin2002-cs@users.noreply.github.com>
2022-09-13 22:31:58 +0530

    5th try

--- Main.py
+++ Main.py
@@ -13,7 +13,7 @@ def insertionSort(array) -> List[int]:
       j=i-1
       while(j>=0 and temp<array[j]):
         array[j+1]=array[j]
-        j--
+        j=j-1
       array[j+1]=temp
    return array
       
