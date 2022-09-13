3fa9b9278f1ee4a4990a5509a1ddedcff533af24
Melvin2002-cs <110010657+Melvin2002-cs@users.noreply.github.com>
2022-09-13 22:30:44 +0530

    4th try

--- Main.py
+++ Main.py
@@ -1,29 +1,21 @@
 from typing import List
 
-def swap(arr,j,k):
-  temp=arr[j]
-  arr[j]=arr[k]
-  arr[k]=temp
-             
+
 
 
 
 
 def insertionSort(array) -> List[int]:
-  for i in range(0,len(array)-1):
-    temp=i
-    execution=0
-    for j in range(i,-1,-1):
-      if execution==0:
-        if array[i]<array[j]:
-          swap(array,i,j)
-          execution=1
-        else:
-          array[j+1]=array[j]
-  return array
-      
-      
-      
+  if len(array)==1:
+    return array
+  for i in range(1,len(array)):
+      temp=array[i]
+      j=i-1
+      while(j>=0 and temp<array[j]):
+        array[j+1]=array[j]
+        j--
+      array[j+1]=temp
+   return array
       
       
 # data = [9, 5, 1, 4, 3]
